5eece7e4d54d81cae84f3fed84a59cadf88ef0b2
rbaron01 <bbrroberto1994@gmail.com>
2019-11-25 17:27:20 +0100

    aggiunta javadoc

--- Rbaron01/src/main/java/upem/server/connection/RMIUPEMServiceManager.java
+++ Rbaron01/src/main/java/upem/server/connection/RMIUPEMServiceManager.java
@@ -6,7 +6,7 @@ public class RMIUPEMServiceManager {
 	
 	/* Il crée un connection RMI sur la porte 4444 
 	 * et ajoute dans le registry un objet UpemService
-	 * avec identifie uperService
+	 * avec  l'identifie uperService
 	 * @return	Un objet qui rappresente une connection en utilisant le protocol RMI 
 	 */
 	
